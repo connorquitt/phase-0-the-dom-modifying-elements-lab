e73c9b526616bd1ed4fc695a88ea64701d6c112d
connorq <connorquitt@gmail.com>
2023-07-10 15:01:58 -0500

    dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.textContent = "cmquitt2001 is the champion";
